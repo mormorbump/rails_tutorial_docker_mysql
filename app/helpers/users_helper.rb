889cb9cf7c82c473ef75cb32c93f0f3ffb6c3d2a
mormorbump <mormorbump@gmail.com>
2019-06-09 16:10:22 +0900

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,10 @@
 module UsersHelper
+
+    def gravatar_for(user, size: 80)
+        # ::（二重コロン）もメソッドの呼び出し。の他に定数の呼び出しも孕んでいる。
+        # Rubyでは， クラスやモジュールも定数
+        gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+        gravatar_url =  "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+        image_tag(gravatar_url, alt: user.name, class: "gravatar")
+    end
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,39 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "invalid signup information" do
+    get signup_path
+    # postでブロック内部のパラムスを送っても、User.countに変化がない事を確認。
+    assert_no_difference 'User.count' do
+      post signup_path, params: { user: {
+        name: "",
+        email: "user@invalid",
+        password: "foo",
+        password_confirmation: "bar"
+      }}
+    end
+    assert_template "users/new"
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert'
+    assert_select 'form[action="/signup"]'
+  end
+
+  test "valid signup information" do
+    get signup_path
+    # postでparams送って、User.countが一つ変わる事を確認。
+    assert_diffrence "User.count", 1 do
+      post signup_path, params: { user: {
+        name: "Example User",
+        email: "user@example.com",
+        password: "password",
+        password_confirmation: "password"
+      }}
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert_not flash.nil?
+  end
+end
